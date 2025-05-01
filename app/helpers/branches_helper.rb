d2f1758afeb470ed7f88b2f6cd731442afbdf134
marcobeffa <marcobeffa@gmail.com>
2025-05-01 11:41:01 +0200

    aggiornato vista video e l'helper che è diverso da helper che c'è su note.flowpulse

--- app/helpers/branches_helper.rb
+++ app/helpers/branches_helper.rb
@@ -143,4 +143,33 @@ module BranchesHelper
     md = Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
     md.render(text).html_safe
   end
+
+  # video
+  def embed_url(content)
+    url = extract_video_url(content)
+    return nil if url.blank?
+
+    if url.include?("youtu.be")
+      id = URI.parse(url).path[1..]
+      "https://www.youtube.com/embed/#{id}"
+    elsif url.include?("youtube.com")
+      id = CGI.parse(URI.parse(url).query.to_s)["v"]&.first
+      "https://www.youtube.com/embed/#{id}" if id
+    elsif url.include?("vimeo.com")
+      id = url[%r{vimeo\.com/(\d+)}, 1]
+      "https://player.vimeo.com/video/#{id}" if id
+    else
+      nil
+    end
+  end
+
+  def extract_video_url(content)
+    if content.respond_to?(:video_url)
+      content.video_url
+    elsif content.is_a?(Hash)
+      content["video_url"] || content[:video_url]
+    else
+      nil
+    end
+  end
 end
--- app/helpers/trees_helper.rb
+++ app/helpers/trees_helper.rb
@@ -1,2 +1,29 @@
 module TreesHelper
+  def embed_url(content)
+    url = extract_video_url(content)
+    return nil if url.blank?
+
+    if url.include?("youtu.be")
+      id = URI.parse(url).path[1..]
+      "https://www.youtube.com/embed/#{id}"
+    elsif url.include?("youtube.com")
+      id = CGI.parse(URI.parse(url).query.to_s)["v"]&.first
+      "https://www.youtube.com/embed/#{id}" if id
+    elsif url.include?("vimeo.com")
+      id = url[%r{vimeo\.com/(\d+)}, 1]
+      "https://player.vimeo.com/video/#{id}" if id
+    else
+      nil
+    end
+  end
+
+  def extract_video_url(content)
+    if content.respond_to?(:video_url)
+      content.video_url
+    elsif content.is_a?(Hash)
+      content["video_url"] || content[:video_url]
+    else
+      nil
+    end
+  end
 end
